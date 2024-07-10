1b3d9d44d2cf3754e62f044c5bbe0a297ae1996c
aameliazn <aameliazulkarnaen@gmail.com>
2024-07-10 12:42:13 +0700

    update

--- resources/views/frontend/portofolio-b.blade.php
+++ resources/views/frontend/portofolio-b.blade.php
@@ -1,104 +1,107 @@
 @extends('layouts.main-b')
 @section('title-head', $mainMenu[2]->title)
 @section('content')
- <!-- agency_heading_start -->
- <div class="agency_heading">
-  <div class="container">
-      <div class="row">
-          <div class="col-xl-12">
-              <div class="agency_text">
-                  <h3>Check Our Awesome Stuffs we build <br>
-                          Up for Business and Brands</h3>
-              </div>
-          </div>
-      </div>
-  </div>
-  <div class="animated_shape">
-      <div class="anim_1">
-          <img src="img/about/1.png" alt="">
-      </div>
-      <div class="anim_2">
-          <img src="img/about/2.png" alt="">
-      </div>
-  </div>
-</div>
-<!-- agency_heading_end -->
+    <!-- agency_heading_start -->
+    <div class="agency_heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="agency_text_porto">
+                        <h3>Jelajahi Ragam Solusi Terbaik yang Kami Tawarkan</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="animated_shape">
+            <div class="anim_1">
+                <img src="img/about/1.png" alt="">
+            </div>
+            <div class="anim_2">
+                <img src="img/about/2.png" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- agency_heading_end -->
 
-<!-- works_area_start -->
-<div class="works_area">
-  <h1 class="opacity_text opacity_text2 d-none d-lg-block">Projects</h1>
-  <div class="container">
-      <div class="row">
-          <div class="col-xl-5 col-md-6">
-              <div class="single_work">
-                  <div class="work_thumb">
-                      <img src="{{asset('portofolioimg/'.$portofolios[0]->image)}}" alt="{{$portofolios[0]->judul}}">
-                      <div class="work_hover">
-                          <div class="work_inner">
-                            <a href="{{route('detail.porto', $portofolios[0]->slug)}}">View Details</a>
+    <!-- works_area_start -->
+    <div class="works_area">
+        <h1 class="opacity_text opacity_text2 d-none d-lg-block">Projects</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-md-6">
+                    <div class="single_work">
+                        <div class="work_thumb">
+                            <img src="{{ asset('portofolioimg/' . $portofolios[0]->image) }}"
+                                alt="{{ $portofolios[0]->judul }}">
+                            <div class="work_hover">
+                                <div class="work_inner">
+                                    <a href="{{ route('detail.porto', $portofolios[0]->slug) }}">View Details</a>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                  </div>
-                  <div class="work_heading">
-                      <h3>{{$portofolios[0]->judul}}</h3>
-                  </div>
-              </div>
-              <div class="single_work">
-                  <div class="work_thumb">
-                      <img src="{{asset('portofolioimg/'.$portofolios[1]->image)}}" alt="{{$portofolios[1]->judul}}">
-                      <div class="work_hover">
-                          <div class="work_inner">
-                            <a href="{{route('detail.porto', $portofolios[1]->slug)}}">View Details</a>
+                        <div class="work_heading">
+                            <h3>{{ $portofolios[0]->judul }}</h3>
                         </div>
-                      </div>
-                  </div>
-                  <div class="work_heading">
-                      <h3>{{$portofolios[1]->judul}}</h3>
-                  </div>
-              </div>
-          </div>
-          <div class="col-xl-5 offset-xl-2 col-md-6">
-              <div class="single_work spacec-top">
-                  <div class="work_thumb">
-                      <img src="{{asset('portofolioimg/'.$portofolios[2]->image)}}" alt="{{$portofolios[2]->judul}}">
-                      <div class="work_hover">
-                          <div class="work_inner">
-                              <a href="{{route('detail.porto', $portofolios[2]->slug)}}">View Details</a>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="work_heading">
-                      <h3>{{$portofolios[2]->judul}}</h3>
-                  </div>
-              </div>
-              <div class="single_work">
-                  <div class="work_thumb">
-                      <img src="{{asset('portofolioimg/'.$portofolios[3]->image)}}" alt="{{$portofolios[3]->judul}}">
-                      <div class="work_hover">
-                          <div class="work_inner">
-                              <a href="{{$portofolios[3]->judul}}">View Details</a>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="work_heading">
-                      <h3>{{$portofolios[3]->judul}}</h3>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-xl-12">
-              <div class="more_products text-center">
-                  <a class="boxed_btn_round" href="#">More Products</a>
-              </div>
-          </div>
-      </div>
-  </div>
-</div>
-<!-- works_area_end -->
+                    </div>
+                    <div class="single_work">
+                        <div class="work_thumb">
+                            <img src="{{ asset('portofolioimg/' . $portofolios[1]->image) }}"
+                                alt="{{ $portofolios[1]->judul }}">
+                            <div class="work_hover">
+                                <div class="work_inner">
+                                    <a href="{{ route('detail.porto', $portofolios[1]->slug) }}">View Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="work_heading">
+                            <h3>{{ $portofolios[1]->judul }}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 offset-xl-2 col-md-6">
+                    <div class="single_work spacec-top">
+                        <div class="work_thumb">
+                            <img src="{{ asset('portofolioimg/' . $portofolios[2]->image) }}"
+                                alt="{{ $portofolios[2]->judul }}">
+                            <div class="work_hover">
+                                <div class="work_inner">
+                                    <a href="{{ route('detail.porto', $portofolios[2]->slug) }}">View Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="work_heading">
+                            <h3>{{ $portofolios[2]->judul }}</h3>
+                        </div>
+                    </div>
+                    <div class="single_work">
+                        <div class="work_thumb">
+                            <img src="{{ asset('portofolioimg/' . $portofolios[3]->image) }}"
+                                alt="{{ $portofolios[3]->judul }}">
+                            <div class="work_hover">
+                                <div class="work_inner">
+                                    <a href="{{ $portofolios[3]->judul }}">View Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="work_heading">
+                            <h3>{{ $portofolios[3]->judul }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="more_products text-center">
+                        <a class="boxed_btn_round" href="#">More Products</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- works_area_end -->
 
-<!-- counter_area_start -->
-{{-- <div class="counter_area minus_padding">
+    <!-- counter_area_start -->
+    {{-- <div class="counter_area minus_padding">
   <h1 class="opacity_text d-none d-lg-block">
           Quick Fact
   </h1>
@@ -125,53 +128,53 @@
       </div>
   </div>
 </div> --}}
-<!-- counter_area_end -->
+    <!-- counter_area_end -->
 
-<!-- instragram_area_start -->
-<div class="instragram_area">
-      <div class="single_instagram">
-          <img src="img/instragram/1.png" alt="">
-          <div class="ovrelay">
-              <a href="#">
-                  <i class="fa fa-instagram"></i>
-              </a>
-          </div>
-      </div>
-      <div class="single_instagram">
-          <img src="img/instragram/2.png" alt="">
-          <div class="ovrelay">
-              <a href="#">
-                  <i class="fa fa-instagram"></i>
-              </a>
-          </div>
-      </div>
-      <div class="single_instagram">
-          <img src="img/instragram/3.png" alt="">
-          <div class="ovrelay">
-              <a href="#">
-                  <i class="fa fa-instagram"></i>
-              </a>
-          </div>
-      </div>
-      <div class="single_instagram">
-          <img src="img/instragram/4.png" alt="">
-          <div class="ovrelay">
-              <a href="#">
-                  <i class="fa fa-instagram"></i>
-              </a>
-          </div>
-      </div>
-      <div class="single_instagram">
-          <img src="img/instragram/5.png" alt="">
-          <div class="ovrelay">
-              <a href="#">
-                  <i class="fa fa-instagram"></i>
-              </a>
-          </div>
-      </div>
-  </div>
-<!-- instragram_area_end -->
-<div class="Visit_Work text-center">
-  <a href="#" class="Visit_link">Visit Our Work</a>
-</div>
+    <!-- instragram_area_start -->
+    <div class="instragram_area">
+        <div class="single_instagram">
+            <img src="img/instragram/1.png" alt="">
+            <div class="ovrelay">
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="single_instagram">
+            <img src="img/instragram/2.png" alt="">
+            <div class="ovrelay">
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="single_instagram">
+            <img src="img/instragram/3.png" alt="">
+            <div class="ovrelay">
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="single_instagram">
+            <img src="img/instragram/4.png" alt="">
+            <div class="ovrelay">
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+        <div class="single_instagram">
+            <img src="img/instragram/5.png" alt="">
+            <div class="ovrelay">
+                <a href="#">
+                    <i class="fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- instragram_area_end -->
+    <div class="Visit_Work text-center">
+        <a href="#" class="Visit_link">Visit Our Work</a>
+    </div>
 @endsection
